c8a7021ec594742fbbd8a5cb2f3432fa01107d8d
sajal2110 <91719202+sajal2110@users.noreply.github.com>
2025-03-01 18:53:08 +0530

    Delete macros directory

--- macros/variables.sql
+++
@@ -1,8 +0,0 @@
-{%macro learn_variables() %}
-
-    {%  set your_name_='Sajal Vishwakarma'%}
-    {{ log("Hello, Jinja variable "~ your_name_,info=True)}}
-
-    {{ log("Hi..! DBT user.., " ~ var("user_name","Default value is ...") ~ "\t hope you are doing well", info=True)}}
-
-{% endmacro %}
